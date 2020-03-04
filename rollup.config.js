d0d5e1cfaba355c13263c952a7a4f5497bc6f099
christian@krey.io <christian@krey.io>
2020-03-04 23:12:58 +0100

    Added build step to github action and files key to package.json.

--- rollup.config.js
+++ rollup.config.js
@@ -6,8 +6,16 @@ const config = {
   input: "src/index.js",
   output: [
     {
+      file: pkg.main,
+      format: "cjs"
+    },
+    {
       file: pkg.module,
       format: "esm"
+    },
+    {
+      file: pkg.browser,
+      format: "umd"
     }
   ],
   plugins: [
