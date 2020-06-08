0279a3cd345c0848720a8c56fb382469451e5545
zeekrey <christian@krey.io>
2020-06-08 15:08:18 +0200

    Added tiny error handling.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,50 @@
-import React from 'react'
+import React from "react";
 
 function useGoogleTagManager(id, conf) {
-  const dataLayerName = conf.dataLayerName || 'dataLayer' 
-  const beSilent = conf.debugging || false
+  const dataLayerName = conf.dataLayerName || "dataLayer";
+  const beSilent = conf.debugging || false;
   const log = (lvl, msg) =>
-    beSilent && console[lvl](`[GoogleTagManager]: ${msg}`)
-  const [dataLayer, setDataLayer] = React.useState(null)
+    beSilent && console[lvl](`[GoogleTagManager]: ${msg}`);
+  const [dataLayer, setDataLayer] = React.useState(null);
 
-  function checkParams(id, conf) {
+  try {
     if (id === undefined || id.length === 0 || !id.match(/^GTM-/)) {
-      log('error', 'ID should be provided.')
-      return false
+      throw new ValidationExcaption(
+        "A Google Tag Manager ID should be provided."
+      );
     } else if (conf) {
       for (const [key, value] of Object.entries(conf)) {
-        if (key !== 'dataLayerName' && key !== 'debugging') {
-          console.log(key)
-          log('error', `The only keys allowed are 'dataLayerName' and 'debugging'.`)
-          return false
+        if (key !== "dataLayerName" && key !== "debugging") {
+          throw new ValidationExcaption(
+            "The only keys allowed are 'dataLayerName' and 'debugging'."
+          );
         } else if (value.length === 0) {
-          log('error', `If you provide ${key}, then it shouldn't be empty.`)
-          return false
+          throw new ValidationExcaption(
+            `If you provide ${key}, then it shouldn't be empty.`
+          );
         }
       }
     }
-    return true
+  } catch (error) {
+    log('error', error)
   }
-
-  if (!checkParams(id, conf)) return {}
 
   React.useEffect(() => {
     if (!window[dataLayerName]) {
       log(
-        'warn',
+        "warn",
         `Google Tag Manager not found. Trying to init GTM with: ${id}.`
-      )
-      initScripts(id, dataLayerName)
+      );
+      initScripts(id, dataLayerName);
     } else {
-      log('info', 'Google Tag Manager found.')
-      setDataLayer(window.dataLayerName)
+      log("info", "Google Tag Manager found.");
+      setDataLayer(window.dataLayerName);
     }
-  }, [])
+  }, []);
 
   React.useEffect(() => {
-    setDataLayer(window[dataLayerName])
-  }, [window[dataLayerName]])
+    setDataLayer(window[dataLayerName]);
+  }, [window[dataLayerName]]);
 
   /**
    * Function to add data to the dataLayer.
@@ -54,14 +55,14 @@ function useGoogleTagManager(id, conf) {
   const gtmData = (data) => {
     if (data === undefined || data.length === 0) {
       log(
-        'error',
+        "error",
         "Data should be provided in the following form: {'varname': 'data'}"
-      )
-      return false
+      );
+      return false;
     } else {
-      return dataLayer.push(data)
+      return dataLayer.push(data);
     }
-  }
+  };
 
   /**
    * Function to trigger an event.
@@ -73,41 +74,50 @@ function useGoogleTagManager(id, conf) {
     if (
       event === undefined ||
       event.length === 0 ||
-      typeof event !== 'string'
+      typeof event !== "string"
     ) {
-      log('error', 'Event data should be a string.')
-      return false
+      log("error", "Event data should be a string.");
+      return false;
     } else {
-      return dataLayer.push({ event: event })
+      return dataLayer.push({ event: event });
     }
-  }
+  };
 
-  return { dataLayer, gtmData, gtmEvent }
+  return { dataLayer, gtmData, gtmEvent };
 }
 
 const initScripts = (id, dataLayerName) => {
   /**
    * As described: https://developers.google.com/tag-manager/quickstart
    */
-  const gtmHeadScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','${dataLayerName}','${id}');`
-  const gtmBodyScript = `<iframe src="https://www.googletagmanager.com/ns.html?id=${id}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+  const gtmHeadScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','${dataLayerName}','${id}');`;
+  const gtmBodyScript = `<iframe src="https://www.googletagmanager.com/ns.html?id=${id}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
 
-  if (typeof document !== 'undefined') {
-    document.head.prepend(addElement('script', gtmHeadScript))
-    document.body.prepend(addElement('noscript', gtmBodyScript))
-    return window ? window[dataLayerName] : true
+  if (typeof document !== "undefined") {
+    document.head.prepend(addElement("script", gtmHeadScript));
+    document.body.prepend(addElement("noscript", gtmBodyScript));
+    return window ? window[dataLayerName] : true;
   }
 
-  return true
-}
+  return true;
+};
 
 /**
- * Small helper function.
+ * Small helper functions.
  */
 const addElement = (nodeType, innerHTML) => {
-  const el = document.createElement(nodeType)
-  el.innerHTML = innerHTML
-  return el
+  const el = document.createElement(nodeType);
+  el.innerHTML = innerHTML;
+  return el;
+};
+
+function ValidationExcaption(message) {
+  this.message = message;
+  this.name = "ValidationException";
 }
 
-export { useGoogleTagManager as default }
+ValidationExcaption.prototype.toString = function () {
+  return `${this.name}: "${this.message}"`;
+};
+
+export { useGoogleTagManager as default };
